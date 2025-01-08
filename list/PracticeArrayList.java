0213d894a22d64297a32791258a2764a0b7c715f
ashwatShiv <ashwatshivharetemp@gmail.com>
2025-01-08 20:12:28 +0000

    intialSetup

---
+++ list/PracticeArrayList.java
@@ -0,0 +1,15 @@
+package list;
+import java.util.*;
+
+
+public class PracticeArrayList {
+public static void main(String[] args) {
+    ArrayList<String> ls = new ArrayList<>();
+    ls.add("ashwat");
+    ls.add("ashish");
+    ls.add("abhay");
+    
+
+}
+    
+}
